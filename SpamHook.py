6bd9e4c34ffd63ef78b72bb6a7643679caf010a3
dat-insanity <54964664+dat-insanity@users.noreply.github.com>
2021-03-05 21:14:28 +1100

    Updated SpamHook to v1.2, and added a wordlist to go with it

--- SpamHook.py
+++ SpamHook.py
@@ -3,7 +3,11 @@
 # GitHub: https://github.com/dat-insanity
 # Discord dat_insanity#2048
 
-import requests, os, sys, time, random
+import requests
+import os 
+import sys 
+import time
+import random
 
 if sys.platform.startswith('win32'):
 	os.system('cls')
@@ -17,13 +21,13 @@ print("\33[35m `--. \ '_ \ / _` | '_ ` _ \|  _  |/ _ \ / _ \| |/ /")
 print('\33[35m/\__/ / |_) | (_| | | | | | | | | | (_) | (_) |   < ')
 print("\33[35m\____/| .__/ \__,_|_| |_| |_\_| |_/\___/ \___/|_|\_\\")
 print('\33[35m	| |                      made by dat_insanity :)')
-print('\33[35m	|_|                                         v1.0')
+print('\33[35m	|_|                                         v1.1')
 print()
 
 webhook = input('\33[31mPaste in your webhook URL: ')
 username = input('\33[31mInput the bot username: ')
 amount = int(input('\33[31mHow many times should the message send (input a negative number for unlimited): '))
-option =  input('\33[31mWhat mode do you pick, custom message (1), or ascii spammer (2): ') 
+option =  input('\33[31mWhat mode do you pick, custom message (1), ascii spammer (2), or wordlist spammer (3): ') 
 
 if option == '1':
 	msg = input('\33[31mPaste in your message: ')
@@ -43,6 +47,7 @@ if option == '1':
 		else:
 			print("\33[40m\033[92mMessage sent successfully, code {}.".format(result.status_code))	
 			amount = amount - 1
+			
 elif option == '2':
 	input('\033[1mPress enter to start, or control-c to quit: ')
 	def ascii(length):
@@ -67,6 +72,37 @@ elif option == '2':
 		else:
 			print("\33[40m\033[92mMessage sent successfully, code {}.".format(result.status_code))	
 			amount = amount - 1
+			
+elif option == '3':
+	input('\33[31mPut your text into wordlist.txt. Hit enter when you\'re done')
+	f = open("wordlist.txt", "r")
+	lines = 0
+	for line in f:
+		if line != "\n":
+			lines += 1
+	f.close()
+	while amount != 0:
+		f = open("wordlist.txt")
+		randline = random.randrange(lines - 1)
+		convrandline = [randline]
+		for position, line in enumerate(f):
+			if position in convrandline:
+				data = {
+					"content" : '@everyone' + line,
+					"username" : username
+				}
+		f.close()
+		result = requests.post(webhook, json = data)
+		try:
+			result.raise_for_status()
+		except requests.exceptions.HTTPError as err:
+			print(err)
+			print('\33[40m\033[91mPausing for a second, you may be rate limited. (usually clears up under a minute)')
+			time.sleep(1)
+		else:
+			print("\33[40m\033[92mMessage sent successfully, code {}.".format(result.status_code))	
+			amount = amount - 1
+			
 else:
 	print('Invalid option chosen for mode, quitting!')
 	quit()
